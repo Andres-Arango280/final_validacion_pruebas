e9502fa34090ec146ec5c91cedcc6f567c076b04
andres-arango280 <brahianosorio21@gmail.com>
2026-06-11 03:30:57 +0000

    fix: ajustes finales cucumber US05

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,3 +1,14 @@
+# features/support/env.rb
 require 'cucumber/rails'
+require 'fabrication'
+
 Capybara.default_driver = :rack_test
 Capybara.default_max_wait_time = 10
+
+Before do
+  puts "\n--- Iniciando escenario ---"
+end
+
+After do
+  puts "--- Escenario completado ---\n"
+end
